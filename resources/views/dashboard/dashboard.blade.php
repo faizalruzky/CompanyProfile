e278e8e43aa942be0b99976c6e7e7d1a2aba467a
Faizal Ruzky <faizalruzky@gmail.com>
2017-05-27 12:31:16 +0700

    add submenu dashboard
    add submenu dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,214 +1,35 @@
 @extends('layouts/dashboard.appdashboard')
 @section('content-dashboard')
 <div class="wrapper">
-    <div class="sidebar" data-color="blue" data-image="assets/img/sidebar-5.jpg">
+	<div class="sidebar" data-color="blue" data-image="assets/img/sidebar-5.jpg">
 
-    <!--
+	<!--
 
-        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
-        Tip 2: you can also add an image using data-image tag
+	Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+	Tip 2: you can also add an image using data-image tag
 
-    -->
+	-->
+	@include('dashboard/shared.side_nav')
 
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="#" class="simple-text">
-                    SMS 
-                </a>
-            </div>
+	</div>
 
-            <ul class="nav">
-                <li class="active">
-                    <a href="dashboard.html">
-                        <i class="pe-7s-graph"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="user.html">
-                        <i class="pe-7s-user"></i>
-                        <p>User Profile</p>
-                    </a>
-                </li>
+	<div class="main-panel">
+		@include('dashboard/shared.top_nav')
 
-                <li><a href="#hse" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-date"> HSE</span><i class="pe-7s-angle-down"></i></a>
-                    <div class="collapse" id="hse">
-                        <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Sub menu</p></a>
-                    </div>
-                </li>
+		<div class="content">
+			<div class="container-fluid">
 
-                <li><a href="#labor" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-help2"> Labor</span><i class="pe-7s-angle-down"></i></a>
-                    <div class="collapse" id="labor">
-                        <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Sub menu</p></a>
-                    </div>
-                </li>
-
-                <li><a href="#HS" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-display1"> HS</span><i class="pe-7s-angle-down"></i></a>
-                    <div class="collapse" id="HS">
-                        <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Sub menu</p></a>
-                    </div>
-                </li>
-
-                <li><a href="#ES" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-leaf">   ES</span><i class="pe-7s-angle-down"></i></a>
-                    <div class="collapse" id="ES">
-                        <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Sub menu</p></a>
-                    </div>
-                </li>
-
-                <li>
-                    <a href="typography.html">
-                        <i class="pe-7s-news-paper"></i>
-                        <p>Typography</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="icons.html">
-                        <i class="pe-7s-science"></i>
-                        <p>Icons</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="maps.html">
-                        <i class="pe-7s-map-marker"></i>
-                        <p>Maps</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="notifications.html">
-                        <i class="pe-7s-bell"></i>
-                        <p>Notifications</p>
-                    </a>
-                </li>
-            </ul>
-    	</div>
-    </div>
-
-    <div class="main-panel">
-        <nav class="navbar navbar-default navbar-fixed">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Dashboard</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-left">
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="fa fa-dashboard"></i>
-								<p class="hidden-lg hidden-md">Dashboard</p>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-globe"></i>
-                                    <b class="caret hidden-sm hidden-xs"></b>
-                                    <span class="notification hidden-sm hidden-xs">5</span>
-									<p class="hidden-lg hidden-md">
-										5 Notifications
-										<b class="caret"></b>
-									</p>
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a href="#">Notification 1</a></li>
-                                <li><a href="#">Notification 2</a></li>
-                                <li><a href="#">Notification 3</a></li>
-                                <li><a href="#">Notification 4</a></li>
-                                <li><a href="#">Another notification</a></li>
-                              </ul>
-                        </li>
-                        <li>
-                           <a href="">
-                                <i class="fa fa-search"></i>
-								<p class="hidden-lg hidden-md">Search</p>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- <li>
-                           <a href="">
-                               <p>Account</p>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <p>
-										Dropdown
-										<b class="caret"></b>
-									</p>
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                              </ul>
-                        </li>
- -->
-                        <li class="dropdown">
-                            @if (Auth::guest())
-                            @else
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                 {{ Auth::user()->name }}
-                                 <b class="caret"></b>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a href="{{url('/logout')}}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>                           
-                            </ul>
-                            @endif
-                        </li>
-						<li class="separator hidden-lg hidden-md"></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+			</div>
+		</div>
 
 
-        <div class="content">
-            <div class="container-fluid">
-                
-            </div>
-        </div>
+		<footer class="footer">
+			@include('dashboard/shared.footer_nav')
+		</footer>
 
-
-        <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Company
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Portfolio
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                               Blog
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <p class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
-                </p>
-            </div>
-        </footer>
-
-    </div>
+	</div>
 </div>
 @endsection
 
- 
+
+
---
+++ resources/views/dashboard/shared/side_nav.blade.php
@@ -0,0 +1,112 @@
+<div class="sidebar-wrapper">
+    <div class="logo">
+        <a href="#" class="simple-text">
+            SMS 
+        </a>
+    </div>
+
+    <ul class="nav">
+        <li class="active">
+            <a href="dashboard.html">
+                <i class="pe-7s-graph"></i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+        <li>
+            <a href="user.html">
+                <i class="pe-7s-user"></i>
+                <p>User Profile</p>
+            </a>
+        </li>
+
+        <li><a href="#hse" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-date"> HSE</span><i class="pe-7s-angle-down"></i></a>
+            <div class="collapse" id="hse">
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Clinic Visit</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>MCU Scedule</p></a>
+
+   
+                    <a href="#PregnantWomen" data-toggle="collapse" data-parent="#PregnantWomen" class="list-group-item"><i class="pe-7s-angle-down"></i><p>Pregnant Women</p></a>
+                    <div class="collapse list-group-submenu" id="PregnantWomen">
+                        <a href="#" class="list-group-item" data-parent="#PregnantWomen"><i class="pe-7s-note2"></i><p>Acc, not acc job</p></a>
+
+                        <a href="#" class="list-group-item" data-parent="#PregnantWomen"><i class="pe-7s-note2"></i><p>Nutrition Training</p></a>
+
+                        <a href="#" class="list-group-item" data-parent="#PregnantWomen"><i class="pe-7s-note2"></i><p>Pregnant Women Exercise</p></a>
+                    </div>
+
+           
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Accident Report</p></a>
+            </div>
+        </li>
+
+        <li><a href="#labor" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-help2"> Labor</span><i class="pe-7s-angle-down"></i></a>
+            <div class="collapse" id="labor">
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>H&E Case</p></a>
+            </div>
+        </li>
+
+        <li><a href="#HS" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-display1"> HS</span><i class="pe-7s-angle-down"></i></a>
+            <div class="collapse" id="HS">
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Near miss program</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>HS Vision</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>HS Goal</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Strategic Plan</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>CSR Related with HS</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Safety Kaizen</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>HS Agent</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Manufacture Modernization</p></a>
+            </div>
+        </li>
+
+        <li><a href="#ES" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-leaf">   ES</span><i class="pe-7s-angle-down"></i></a>
+            <div class="collapse" id="ES">
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>ES North start goal</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Company Vision & Mission</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>ES Member PT.CSI </p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Waste Management</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Water Management</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Hazardous Waste Management</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Annual Target</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Strategic Plan 3-5 years</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Employee's Contribution</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>CV, TNA, IDP of ES Team</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>ES Technology and Inovation</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Engagement with supplier</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Enviromental Aspect Inventory</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>OTP</p></a>
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>ES Green Team</p></a>
+            </div>
+        </li>
+
+        <li><a href="#EC" data-toggle="collapse" data-parent="#MainMenu"><span class="pe-7s-sun">   EC</span><i class="pe-7s-angle-down"></i></a>
+            <div class="collapse" id="EC">
+                <a href="#" class="list-group-item"><i class="pe-7s-note2"></i><p>Energy Used/plant</p></a>
+            </div>
+        </li>
+
+        <li>
+            <a href="typography.html">
+                <i class="pe-7s-news-paper"></i>
+                <p>Typography</p>
+            </a>
+        </li>
+        <li>
+            <a href="icons.html">
+                <i class="pe-7s-science"></i>
+                <p>Icons</p>
+            </a>
+        </li>
+        <li>
+            <a href="maps.html">
+                <i class="pe-7s-map-marker"></i>
+                <p>Maps</p>
+            </a>
+        </li>
+        <li>
+            <a href="notifications.html">
+                <i class="pe-7s-bell"></i>
+                <p>Notifications</p>
+            </a>
+        </li>
+    </ul>
+</div>
