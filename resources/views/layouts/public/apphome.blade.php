885ec6d1695f88a401a66f3fb2d1f172629c4088
Faizal Ruzky <faizalruzky@gmail.com>
2017-06-02 15:35:28 +0700

    add navigation megamenu
    add navigation megamenu

--- resources/views/layouts/public/apphome.blade.php
+++ resources/views/layouts/public/apphome.blade.php
@@ -8,6 +8,11 @@
     <meta name="keywords" content="jquery, circular menu, navigation, round, bubble"/>
     <link rel="stylesheet" href="assets/public/custom/nav/css/style.css" type="text/css" media="screen"/>
 
+    <!-- megamenu -->
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="assets/public/custom/megamenu/style.css">
+
     <!-- bg -->
     <meta name="keywords" content="css3, css-only, fullscreen, background, slideshow, images, content" />
     <link rel="stylesheet" type="text/css" href="assets/public/custom/bgslide/css/demo.css" />
@@ -24,12 +29,17 @@
         /*background:#fff url(assets/public/custom/nav/images/bg.png) no-repeat top left;*/
     }
     .title{
-        width:548px;
-        height:119px;
+        width:auto;
+        height:auto;
         position:absolute;
         top:400px;
         left:150px;
         background:transparent url(assets/public/custom/nav/title.png) no-repeat top left;
+    }
+    #title{
+        position:absolute;
+        top:400px;
+        left:0px;
     }
     a.back{
         background:transparent url(assets/public/custom/nav/back.png) no-repeat top left;
@@ -56,6 +66,11 @@
 
 </body>
 <!-- The JavaScript -->
+<!-- megamenu -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
+ <script src="assets/public/custom/megamenu/jquery.js"></script>
+
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script type="text/javascript" src="assets/public/custom/nav/jquery.easing.1.3.js"></script>
 <script type="text/javascript">
